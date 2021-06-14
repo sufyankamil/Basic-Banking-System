f2c000a06fba246f201e2c1d4df3560a5d5cd468
Sufyan Kamil <sufyankamil15@gmail.com>
2021-06-14 19:26:32 +0530

    Website Developed

---
+++ README.md
@@ -0,0 +1,15 @@
+# Basic-Bannking-System
+  
+A Web Application used to transfer money between multiple users. A dummy user can also be created.  
+
+# Tech Stack used - 
+Front-end : HTML, CSS, Bootstrap & Javascript 
+Back-end : PHP 
+Database : MySQL   
+
+Database contains two Tables- Users Table & Transaction Table 
+1. User table have basic fields such as name, email & current balance. 
+2. Transaction table records all transfers happened along with their time.  
+
+Flow of the Website: Home Page > View all Users > Select and View one User > Transfer Money > Select reciever > View all Users > View Transfer History.
+
---
+++ transfermoney.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Transfer Money</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/table.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+
+    <style type="text/css">
+      button{
+        transition: 1.5s;
+      }
+      button:hover{
+        background-color:#616C6F;
+        color: white;
+      }
+    </style>
+</head>
+
+<body>
+<?php
+    include 'config.php';
+    $sql = "SELECT * FROM users";
+    $result = mysqli_query($conn,$sql);
+?>
+
+<?php
+  include 'navbar.php';
+?>
+
+<div class="container">
+        <h2 class="text-center pt-4">Transfer Money</h2>
+        <br>
+            <div class="row">
+                <div class="col">
+                    <div class="table-responsive-sm">
+                    <table class="table table-hover table-sm table-striped table-condensed table-bordered">
+                        <thead>
+                            <tr>
+                            <th scope="col" class="text-center py-2">Id</th>
+                            <th scope="col" class="text-center py-2">Name</th>
+                            <th scope="col" class="text-center py-2">E-Mail</th>
+                            <th scope="col" class="text-center py-2">Balance</th>
+                            <th scope="col" class="text-center py-2">Operation</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                <?php 
+                    while($rows=mysqli_fetch_assoc($result)){
+                ?>
+                    <tr>
+                        <td class="py-2"><?php echo $rows['id'] ?></td>
+                        <td class="py-2"><?php echo $rows['name']?></td>
+                        <td class="py-2"><?php echo $rows['email']?></td>
+                        <td class="py-2"><?php echo $rows['balance']?></td>
+                        <td><a href="selecteduserdetail.php?id= <?php echo $rows['id'] ;?>"> <button type="button" class="btn">Transact</button></a></td> 
+                    </tr>
+                <?php
+                    }
+                ?>
+            
+                        </tbody>
+                    </table>
+                    </div>
+                </div>
+            </div> 
+         </div>
+         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+<!------- FOOTER ---------
+<div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="footer-col-1">
+                <h3>Download Our App</h3>
+                <p>Download App for Android & iOS</p>
+                <div class="app-logo">
+                    <img src="img/play-store.png">
+                    <img src="img/app-store.png">
+                </div>
+            </div>
+
+            <div class="footer-col-2">
+                <img src="img/logo.png">
+                <p></p>
+            </div>
+
+            <div class="footer-col-4">
+                <h3>Follow Us</h3>
+                <ul>
+                    <li>Facebook</li>
+                    <li>Twitter</li>
+                    <li>Youtube</li>
+                    <li>Instagram</li>
+                </ul>
+
+            </div>
+        </div>
+        <hr>
+       
+    </div>
+</div>
+-->
+
+</body>
+</html>
